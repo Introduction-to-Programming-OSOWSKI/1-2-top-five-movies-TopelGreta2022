c6ba7a3e52a397acc66d96d06975837d207284f5
TopelGreta2022 <831914@mystma.org>
2021-12-22 09:26:38 -0600

    7

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies( a, b, c, d, e):
-    return  a + b + c + d + e 
+    return  a + " " + b + " " + c + " " + d + " " + e + ""
 
-topFiveMovies("the little mermaid", "bueaty and the beast", "cinderella", "snow white", "adldin")
+topFiveMovies("the little mermaid", "bueaty and the beast", "cinderella", "snow white", "addldin")
